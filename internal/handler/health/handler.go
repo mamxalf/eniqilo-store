c98d439a130b3c15baa32238204bc43c3d8dc685
Hammam Firdaus <hammamxalf@gmail.com>
2024-05-09 08:28:18 +0700

    add docker compose and create ping db health

--- internal/handler/health/handler.go
+++ internal/handler/health/handler.go
@@ -2,16 +2,19 @@ package health
 
 import (
 	"github.com/go-chi/chi"
-	"github.com/mamxalf/eniqilo-store/http/middleware"
+	"github.com/mamxalf/eniqilo-store/infra"
+	"github.com/mamxalf/eniqilo-store/shared/logger"
+	"github.com/mamxalf/eniqilo-store/shared/response"
+	"net/http"
 )
 
 type HealthHandler struct {
-	JWTMiddleware *middleware.JWT
+	DB *infra.PostgresConn
 }
 
-func ProvideHealthHandler(jwt *middleware.JWT) HealthHandler {
+func ProvideHealthHandler(db *infra.PostgresConn) HealthHandler {
 	return HealthHandler{
-		JWTMiddleware: jwt,
+		DB: db,
 	}
 }
 
@@ -19,6 +22,40 @@ func (h *HealthHandler) Router(r chi.Router) {
 	r.Route("/health", func(r chi.Router) {
 		r.Group(func(r chi.Router) {
 			r.Get("/ping", h.Ping)
+			r.Get("/ping-db", h.PingDB)
 		})
 	})
+}
+
+// Ping get health message.
+// @Summary Ping.
+// @Description This endpoint for get health message.
+// @Tags Health
+// @Accept  json
+// @Produce json
+// @Success 200 {object} response.Base
+// @Failure 503 {object} response.Base
+// @Router /v1/health/ping [get]
+func (h *HealthHandler) Ping(w http.ResponseWriter, _ *http.Request) {
+	response.WithMessage(w, http.StatusOK, "PONG!!!")
+}
+
+// PingDB get health message.
+// @Summary PingDB.
+// @Description This endpoint for get health message.
+// @Tags Health
+// @Accept  json
+// @Produce json
+// @Success 200 {object} response.Base
+// @Failure 503 {object} response.Base
+// @Router /v1/health/ping-db [get]
+func (h *HealthHandler) PingDB(w http.ResponseWriter, r *http.Request) {
+	err := h.DB.PG.PingContext(r.Context())
+	if err != nil {
+		logger.WarningWithMessage(err, "Failed PING DB!")
+		response.WithError(w, err)
+		return
+	}
+
+	response.WithMessage(w, http.StatusOK, "DB Postgres is Healthy!")
 }
