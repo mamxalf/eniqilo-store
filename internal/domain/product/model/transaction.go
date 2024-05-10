60b02fb40df5b014c98ad77591cbfdc3b85c9538
yurico12 <mahardika.billian@izeem.com>
2024-05-10 21:25:29 +0700

    feat: add model transaction

---
+++ internal/domain/product/model/transaction.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
+	ID             uuid.UUID       `db:"id" json:"transactionId"`
+	CustomerID     uuid.UUID       `db:"customer_id" json:"customerId"`
+	ProductDetails []ProductDetail `db:"product_details" json:"productDetails"`
+	Paid           int             `db:"paid" json:"paid"`
+	Change         int             `db:"change" json:"change"`
+	CreatedAt      time.Time       `db:"created_at" json:"createdAt"`
+}
+
+type ProductDetail struct {
+	ProductID string `json:"productId"`
+	Quantity  int    `json:"quantity"`
+}
